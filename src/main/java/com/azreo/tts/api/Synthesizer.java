f700d6ce56ee3748d247cdf09b90c7a9eaf4305d
AzReco LLC <47669057+azrecollc@users.noreply.github.com>
2019-10-18 18:02:35 +0400

    Update Synthesizer.java
    Added synthesizing speech direct from text parameter.

--- src/main/java/com/azreo/tts/api/Synthesizer.java
+++ src/main/java/com/azreo/tts/api/Synthesizer.java
@@ -7,9 +7,14 @@ package com.azreo.tts.api;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -18,6 +23,7 @@ import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 /**
@@ -28,7 +34,8 @@ public class Synthesizer {
     private String userId = null;
     private String token = null;
     private String lang = null;
-    private static final String API_URL = "http://api.azreco.az/synthesize";
+    private static final String API_URL_FILE = "http://api.azreco.az/synthesize";
+    private static final String API_URL_TEXT = "http://api.azreco.az/synthesize/text";
     
     public Synthesizer(String userId, String token, String lang) {
         this.userId = userId;
@@ -40,7 +47,7 @@ public class Synthesizer {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         byte[] result = null;
         try {
-            HttpPost httpPost = new HttpPost(API_URL);
+            HttpPost httpPost = new HttpPost(API_URL_FILE);
             FileBody binary = new FileBody(new File(textFile));
             StringBody idContent = new StringBody(userId, ContentType.TEXT_PLAIN);
             StringBody tokenContent = new StringBody(token, ContentType.TEXT_PLAIN);
@@ -87,6 +94,65 @@ public class Synthesizer {
                 }
             }
         }
+        finally {
+            try {
+                httpClient.close();
+            } catch (IOException ex) {
+            }
+        }
+        return result;
+    }
+    
+    public byte[] synthesizeText(String text) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        byte[] result = null;
+        try {
+            HttpPost httpPost = new HttpPost(API_URL_TEXT);
+            httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+            List<NameValuePair> reqEntityParams = new ArrayList<NameValuePair>();
+            reqEntityParams.add(new BasicNameValuePair("api_id", userId));
+            reqEntityParams.add(new BasicNameValuePair("api_token", token));
+            reqEntityParams.add(new BasicNameValuePair("lang", lang));
+            reqEntityParams.add(new BasicNameValuePair("text", text));
+            httpPost.setEntity(new UrlEncodedFormEntity(reqEntityParams, "utf8"));
+            CloseableHttpResponse response = null;
+            try {
+                response = (CloseableHttpResponse) httpClient.execute(httpPost);
+                if(response.getStatusLine().getStatusCode() != 200) {
+                    if(response.getStatusLine().getStatusCode() == HttpStatus.SC_BAD_REQUEST) {
+                        HttpEntity resEntity = response.getEntity();
+                        String error = EntityUtils.toString(resEntity);
+                        System.err.println("Text-to-speech failed: " + error);
+                    } else {
+                        System.err.println("Text-to-speech error: " + response.getStatusLine().getReasonPhrase());
+                    }
+                    return null;
+                }
+                
+                HttpEntity resEntity = response.getEntity();
+
+                try {
+                    result = EntityUtils.toByteArray(resEntity);
+                    EntityUtils.consume(resEntity);
+                } catch (IOException | ParseException ex) {
+                    System.err.println("Parsing response failed: " + ex.getMessage());
+                }
+            } catch (IOException ex) {
+                System.err.println("Making text-to-speech request failed: " + ex.getMessage());
+                return null;
+            }
+            finally {
+                if(response != null) {
+                    try {
+                        response.close();
+                    } catch (IOException ex) {
+                    }
+                }
+            }
+        }
+        catch (UnsupportedEncodingException ex) {
+            System.err.println("Making text-to-speech request failed: " + ex.getMessage());
+        }        
         finally {
             try {
                 httpClient.close();
